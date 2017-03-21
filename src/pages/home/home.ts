9903e324f998939f3f0220ff860f430db5cdc6bc
Carlos Ruiz <ruizcarlos93@yahoo.es>
2017-03-21 17:05:31 +0100

    added users page to navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { UsersPage } from '../users/users';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,7 +12,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    
+
   }
 
+  onGoToUsers(){
+    this.navCtrl.push(UsersPage);
+  }
 }
